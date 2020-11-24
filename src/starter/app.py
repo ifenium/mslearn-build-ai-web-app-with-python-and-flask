150c749548c45db64d03af7485c07a59a8ee1deb
ife <oyeife@gmail.com>
2020-11-24 11:03:46 +0100

    update

--- src/starter/app.py
+++ src/starter/app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+import base64
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Define route for the app's one and only page
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        # Display the image that was uploaded
+        image = request.files["file"]
+        uri = "data:;base64," + base64.b64encode(image.read()).decode("utf-8")
+
+    else:
+        # Display a placeholder image
+        uri = "/static/placeholder.png"
+
+    return render_template("index.html", image_uri=uri)
